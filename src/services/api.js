bef0e52567a0c52b5ee4b02f36ac7a877fba785a
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-26 14:20:21 -0800

    commented out msw example

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export const fetchBreakingBad = async () => {
   const resp = await fetch(`https://www.breakingbadapi.com/api/characters`);
   const characterData = await resp.json();
-  console.log('characterData', characterData);
+  // console.log('characterData', characterData);
   return characterData;
 };
