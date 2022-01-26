ff630036d13637fc0866844642be6604d87644ce
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-25 17:27:26 -0800

    behavioral test for search bar written

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,15 @@
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', async () => {
+test('app renders components on page', async () => {
   render(<App />);
 
   const loading = screen.getByText(/loading/i);
   expect(loading).toBeInTheDocument();
   await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
   const heading = screen.getByRole('heading', { name: /breaking bad/i });
-  const searchBar = await screen.findByLabelText(/search/i);
+  const searchBar = await screen.findByRole('textbox', { name: /search/i });
   const name = screen.getByRole('heading', { name: /jesse/i });
   const img = screen.getByAltText(/jesse/i);
 
@@ -16,4 +17,24 @@ test('renders learn react link', async () => {
   expect(searchBar).toBeInTheDocument();
   expect(name).toBeInTheDocument();
   expect(img).toBeInTheDocument();
+});
+
+test('searchbar works correctly', async () => {
+  render(<App />);
+
+  // grab searchbar
+  const searchBar = await screen.findByRole('textbox', { name: /search/i });
+
+  // fake name in searchbar
+  const characterName = 'jesse';
+  userEvent.type(searchBar, characterName);
+
+  // grab character results and map
+  const characters = await screen.findAllByText(characterName, { exact: false });
+  const result = characters.map((character) => character.textContent);
+
+  // check that name is equal to all name results
+  const handleCheck = (name) => name.toLowerCase().includes(characterName);
+  const hasSameName = result.every(handleCheck);
+  expect(hasSameName).toBe(true);
 });
